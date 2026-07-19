978a745bab6adb50f649d053c9f6582b380bf728
24042cl <24042cl@hvhs.school.nz>
2026-07-19 07:38:53 +0000

    Jul 19, 2026, 7:38 AM

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ else
  let weekDay = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 
  let choice = Number(WEEK_FIELD.value);
- if (weekDay[0])
+ if (pullRequest.weekDay[0])
 
 
  if (age < 20){
