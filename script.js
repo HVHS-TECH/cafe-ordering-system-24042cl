85c21e833b26dcfaf26589fca678ed0d9db809f5
24042cl <24042cl@hvhs.school.nz>
2026-07-23 07:47:06 +0000

    Jul 23, 2026, 7:47 AM

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ userName = NAME_FIELD.value;
 if (items.length === 0){
       OUTPUT.innerHTML = "<p>You haven't added anything to your cart</p>"
 }*/
-if (){
+if (cartValue <= 0){
       OUTPUT.innerHTML = "<p>You haven't added anything to your cart</p>"
 }
 /*
